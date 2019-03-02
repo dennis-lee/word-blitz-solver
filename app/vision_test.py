6686701a35110d6e481b9e45bfba2a6929c427eb
Dennis Lee <dennis.lcfx@gmail.com>
2019-03-02 15:04:02 +0800

    Added function to remove noise from tile images

--- app/vision_test.py
+++ app/vision_test.py
@@ -1,6 +1,20 @@
 import cv2
 import imutils
+import matplotlib.pyplot as plt
 import numpy as np
+
+
+def extract_letter(image):
+    img = cv2.bitwise_not(image)
+    mask = cv2.inRange(img, np.array([205], dtype=np.uint8), np.array([255], dtype=np.uint8))
+    target = cv2.bitwise_and(img, img, mask=mask)
+    k = np.ones((2, 2), np.uint8)
+    e = cv2.erode(target, k, iterations=2)
+    d = cv2.dilate(e, k, iterations=3)
+    img = cv2.bitwise_not(d)
+
+    return img
+
 
 TARGET = (76, 21, 38)  # Color of the game window
 BONUS_2W = (121, 56, 242)
@@ -11,6 +25,7 @@ upper = np.array([TARGET[0] + 10, TARGET[1] + 10, TARGET[2] + 38], dtype=np.uint
 TILE_LENGTH = 64
 
 image = cv2.imread('resources/screenshot.png')
+image_gray = cv2.imread('resources/screenshot.png', 0)
 
 # Focus detection on game window
 window_mask = cv2.inRange(image, lower, upper)
@@ -35,6 +50,9 @@ erosion = cv2.erode(game_th, kernel, iterations=5)
 contours = cv2.findContours(erosion.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 contours = imutils.grab_contours(contours)
 
+test = None
+tiles = []
+
 for c in contours:
     peri = cv2.arcLength(c, True)
     approx = cv2.approxPolyDP(c, 0.04 * peri, True)
@@ -44,8 +62,25 @@ for c in contours:
         y = approx[0][0][1]
         tile_x = GAME_X + x
         tile_y = GAME_Y + y
-        cv2.rectangle(image, (tile_x, tile_y), (tile_x + 64, tile_y + 64), (0, 255, 0), 2)
+        # cv2.rectangle(image, (tile_x, tile_y), (tile_x + TILE_LENGTH, tile_y + TILE_LENGTH), (0, 255, 0), 1)
+        tile = image_gray[tile_y:tile_y+TILE_LENGTH, tile_x:tile_x+TILE_LENGTH].copy()
+        tile = extract_letter(tile)
+        tiles.append(tile)
+        # break
 
-cv2.imshow('image', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.imshow('image', test)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+titles = None
+n_images = len(tiles)
+if titles is None:
+    titles = ['Image (%d)' % i for i in range(1, n_images + 1)]
+fig = plt.figure()
+for n, (image, title) in enumerate(zip(tiles, titles)):
+    a = fig.add_subplot(4, np.ceil(n_images/float(4)), n + 1)
+    if image.ndim == 2:
+        plt.gray()
+    plt.imshow(image)
+    a.set_title(title)
+fig.set_size_inches(np.array(fig.get_size_inches()) * n_images)
+plt.show()
